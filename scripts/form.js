2e1a3c0e32936f5e625ca5815d9bdc603838dee5
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2025-04-05 22:35:16 -0600

    Audit 2

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,45 @@
-import { printCurrentYear, printLastModified, spinnerShow } from "./Util.mjs";
+/**
+ * This script is used to get the current year and the last modified date of the page.
+ */
+function printCurrentYear(id) {
+  const today = new Date();
+
+  document.getElementById(id).innerHTML = today.getFullYear();
+}
+
+/**
+ * This function prints the last modified date of the page.
+ */
+function printLastModified(id) {
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+
+  const lastModified = new Date(document.lastModified).toLocaleDateString(
+    "en-US",
+    options
+  );
+
+  document.getElementById(id).innerHTML = lastModified;
+}
+
+/**
+ * This function shows or hides a spinner element based on the active parameter.
+ */
+const spinnerShow = (active = true, spinnerId = "spinner") => {
+  const spinner = document.getElementById(spinnerId);
+  if (active) {
+    spinner.style.display = "flex"; // Show spinner
+  } else {
+    spinner.style.display = "none"; // Hide spinner
+  }
+};
 
 const products = [
   {
@@ -32,7 +73,9 @@ const initProducts = () => {
 
   // Create a new option for each product and add it to the select element
   products.forEach((product) => {
-    select.appendChild(new Option(product.name, product.id +" - " + product.name.toUpperCase()));
+    select.appendChild(
+      new Option(product.name, product.id + " - " + product.name.toUpperCase())
+    );
   });
 };
 const init = () => {
